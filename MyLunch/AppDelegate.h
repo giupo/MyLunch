e4ee31eb1569d74565417c13593a612c486028a5
Giuseppe Acito <giuseppe.acito@gmail.com>
2015-02-26 11:49:32 +0100

    Initial Commit

---
+++ MyLunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLunch
+//
+//  Created by Giuseppe Acito on 26/02/15.
+//  Copyright (c) 2015 CoreDumper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLunch/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  MyLunch
+//
+//  Created by Giuseppe Acito on 26/02/15.
+//  Copyright (c) 2015 CoreDumper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
